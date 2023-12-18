93576cfd80d920485a9bcd0eaafc13191c8d1cd7
Michael Sagnes <kvikk92@gmail.com>
2023-12-18 22:58:23 +0100

    Changed route endpoint to enhance consistency

--- routes/aircraft.routes.js
+++ routes/aircraft.routes.js
@@ -5,7 +5,7 @@ const authorize = require('./../middleware/authorize');
 const aircraftController = require('../controllers/aircraft.controller');
 const router = express.Router();
 
-router.get('/overview', authorize.verifyToken, aircraftController.getOverviewPage);
+router.get('/browse', authorize.verifyToken, aircraftController.getOverviewPage);
 router.get('/:id', authorize.verifyToken, aircraftController.getDetailsPage);
 
 module.exports = router;
