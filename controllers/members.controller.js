54cc413f61c53ddb21809416a89ba545d02a16aa
Michael Sagnes <kvikk92@gmail.com>
2023-12-19 00:50:02 +0100

    Added controller function for profile page render

--- controllers/members.controller.js
+++ controllers/members.controller.js
@@ -1,6 +1,28 @@
+const { User }    = require('./../models/user.model');
+const { Country } = require('./../models/country.model');
+const jwt      = require("jsonwebtoken");
+
 exports.getDashboardPage = async (req, res, next) => {
     res.status(200).render('members/dashboard', {
-      pageTitle: 'Skyreach',
+      pageTitle: 'Skyreach - Dashboard',
       path: '/members/dashboard'
     });
+}
+
+exports.getProfilePage = async (req, res, next) => {
+  let token = req.cookies["x-access-token"];
+  if (!token) {
+      return res.status(404).redirect('/page-not-found');
+  }
+
+  var decoded    = await jwt.verify(token, process.env.JWT_SECRET);
+  var user       = await User.findById(decoded.id);
+  var countries  = await Country.find({"isActive": true});
+
+  res.status(200).render('members/profile/details', {
+      pageTitle: 'Skyreach - Profile',
+      path: '/members/profile/',
+      user: user,
+      countries: countries
+  });
 }
--- routes/members.routes.js
+++ routes/members.routes.js
@@ -5,6 +5,7 @@ const membersController = require('../controllers/members.controller');
 const router = express.Router();
 
 router.get('/dashboard', authorize.verifyToken, membersController.getDashboardPage);
+router.get('/profile', authorize.verifyToken, membersController.getProfilePage);
 
 
 module.exports = router;
