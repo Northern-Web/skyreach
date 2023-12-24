81d2972e294e133c5a9e2bcf35a185c4c1bce791
Michael Sagnes <kvikk92@gmail.com>
2023-12-24 13:20:22 +0100

    Moved server initializations to separate loader

---
+++ loaders/Express.js
@@ -0,0 +1,46 @@
+const express    = require('express');
+var bodyParser   = require('body-parser');
+var cookieParser = require('cookie-parser')
+const ejs        = require('ejs');
+
+class ExpressLoader {
+    constructor () {
+        const app = express();
+    
+        // App Uses
+        app.use(express.json());
+        app.use(express.static('public'));
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(cookieParser());
+        app.set('view engine', 'ejs');
+
+        // Router Requirements
+        const authRoutes     = require('./../routes/auth.routes');
+        const aircraftRoutes = require('./../routes/aircraft.routes');
+        const homeRoutes     = require('./../routes/home.routes');
+        const userRoutes     = require('./../routes/user.routes');
+        const skydiveRoutes  = require('./../routes/jump.routes');
+        const membersRoutes  = require('./../routes/members.routes');
+
+        // Route Uses
+        app.use("/auth", authRoutes);
+        app.use("/aircrafts", aircraftRoutes);
+        app.use("/home", homeRoutes);
+        app.use("/users", userRoutes);
+        app.use("/skydives", skydiveRoutes);
+        app.use("/members", membersRoutes);
+
+        // Start application
+        this.server = app.listen( process.env.PORT, () => {
+            console.log(`${process.env.APP_NAME} booting in \"${process.env.APP_ENV}\" mode...`);
+            console.log(`${process.env.APP_NAME} listening on port ${process.env.PORT}`);
+        });
+    }
+
+    get Server () {
+        return this.server;
+    }
+}
+
+module.exports = ExpressLoader;
--- server.js
+++ server.js
@@ -1,49 +1,18 @@
-const express    = require('express');
 const mongoose   = require('mongoose');
-var bodyParser   = require('body-parser');
-var cookieParser = require('cookie-parser')
-const ejs        = require('ejs');
-
 require('dotenv').config();
 
-const app = express();
-
-// App Uses
-app.use(express.json());
-app.use(express.static('public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.set('view engine', 'ejs');
+mongoose.Promise = global.Promise;
 
 
-
-// Router Requirements
-const homeRoutes     = require("./routes/home.routes");
-const authRoutes     = require("./routes/auth.routes");
-const membersRoutes  = require("./routes/members.routes");
-const aircraftRoutes = require("./routes/aircraft.routes");
-const jumpRoutes     = require("./routes/jump.routes");
-const userRoutes     = require("./routes/user.routes");
-
-// Router Uses
-app.use("/", homeRoutes);
-app.use("/auth", authRoutes);
-app.use("/users", userRoutes);
-app.use("/members", membersRoutes);
-app.use("/members/aircrafts", aircraftRoutes);
-app.use("/members/skydives", jumpRoutes);
-
-
-app.listen(process.env.PORT || 3000, () => {
-    mongoose.connect(process.env.DB_URI);
-    console.log(`${process.env.APP_NAME} booting in \"${process.env.APP_ENV}\" mode...`);
-    console.log(`Successful connection to database \"${process.env.DB_NAME}\".`);
-    console.log(`${process.env.APP_NAME} listening on port ${process.env.PORT}`);
-  });
-
-// Handling Server Error
-process.on("unhandledRejection", err => {
-    console.log(`An error occurred: ${err.message}`)
-    server.close(() => process.exit(1))
+  // Connect to the DB an initialize the app if successful
+  mongoose.connect(process.env.DB_URI)
+  .then( () => {
+      console.log(`Successful connection to database \"${process.env.DB_NAME}\".`);
+  
+    // Create express instance to setup API
+    const ExpressLoader = require( "./loaders/Express" );
+    new ExpressLoader();
   })
+  .catch( err => {
+    console.error(err);
+  } );
