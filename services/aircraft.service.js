9fd0888fdd414f846f44f7c2284205e374ae5370
Michael Sagnes <kvikk92@gmail.com>
2023-12-26 17:27:59 +0100

    Added aircraft service

--- services/aircraft.service.js
+++ services/aircraft.service.js
@@ -8,5 +8,12 @@ class AircraftService {
     async GetAircrafts (query, options) {
         return await Aircraft.find(query).setOptions(options);
     }
+
+    async GetAircraftById (id) {
+        if (!id) {
+            throw new Error('No Aircraft ID was provided. Unable to get aircraft.');
+        }
+        return await Aircraft.findById(id);
+    }
 }
 module.exports = AircraftService;
