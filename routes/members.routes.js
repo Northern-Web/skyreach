b5b7a1f0ee06bc5a95681b6ce5d1f3e897b38886
Michael Sagnes <kvikk92@gmail.com>
2023-12-27 16:48:35 +0100

    Feedback page navigation

--- routes/members.routes.js
+++ routes/members.routes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.get('/dashboard', authorize.verifyToken, membersController.getDashboardPage);
 router.get('/profile', authorize.verifyToken, membersController.getProfilePage);
+router.get('/feedback', authorize.verifyToken, membersController.getFeedbackPage);
 
 
 module.exports = router;
